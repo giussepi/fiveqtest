664afba5a5a2c21cf7c1f128554eec7fde0fe299
Giussepi <giussepexy@gmail.com>
2012-10-22 16:11:01 -0500

    I forgot this jiji

--- load_csv.py
+++ load_csv.py
@@ -2,7 +2,7 @@ import csv
 import os
 import sys
 from os.path import dirname
-BASEDIR = os.path.join(dirname(__file__), '../')
+BASEDIR = dirname(__file__)
 
 sys.path.append(os.path.join(BASEDIR))
 os.environ['DJANGO_SETTINGS_MODULE'] = 'fiveqtest.settings'
@@ -13,8 +13,9 @@ csv_filepathname = os.path.join(
 reader = csv.reader(
     open(csv_filepathname), delimiter=',', quotechar='"')
 
-for row in reader:        
-    Daily_Devotional.objects.create(title=row[0],
-                                    month=row[1],
-                                    day=row[2],
-                                    body=row[3])
+for row in reader:
+    if row[0] != 'title':
+        Daily_Devotional.objects.create(title=row[0],
+                                        month=row[1],
+                                        day=row[2],
+                                        body=row[3])
